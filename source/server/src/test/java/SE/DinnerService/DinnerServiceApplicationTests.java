7a6c82ab936aa614d699f97ea08d225d5f952eb6
Chloe <kyjjulia07@gmail.com>
2022-09-28 15:15:21 +0900

    [22/09/28] server_setting
    서버 초기 설정

---
+++ source/server/src/test/java/SE/DinnerService/DinnerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package SE.DinnerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinnerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
