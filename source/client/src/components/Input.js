20f81709b895874ed3882b86fb0fbbbaec509e86
alicehrk <alicehrk23@gmail.com>
2022-11-06 00:49:01 +0900

    Login Page

---
+++ source/client/src/components/Input.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled, {css} from 'styled-components';
+
+const StyledInput = styled.input`
+    border: 1px solid #212121;
+    width: 45%;
+    padding: 12px 4px;
+    margin: 5px;
+    box-sizing: border-box;
+    border-radius: 5px;
+`;
+
+const Input = (props) => {
+    const {placeholder} = props;
+    return (
+        <StyledInput placeholder={placeholder}/>
+    );
+};
+
+export default Input;
--- source/client/src/pages/Login.js
+++ source/client/src/pages/Login.js
@@ -1,11 +1,64 @@
 import React from 'react';
+import styled, {css} from 'styled-components';
+import { Link } from 'react-router-dom';
+import Input from '../components/Input';
+import Button from '../components/Button';
 
-const Login = () => {
+const MainDiv = styled.div`
+  padding: 80px;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ButtonLayout = styled.div`
+  position: relative;
+  width: 25%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 30px;
+`
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  :hover{
+    color: blue;
+  }
+`
+const SignupDiv = styled.div`
+  font-size: 13px;
+`
+
+const Login = (props) => {
   return (
-    <div>
-      <h1>로그인</h1>
-    </div>
+    <MainDiv>
+      <img src='/images/logo_black.svg' alt='logo' width={200}/>
+      <div class=""></div>
+      <Container>
+        <Input placeholder=' 아이디'/>
+        <Input placeholder=' 비밀번호'/>
+      </Container>
+      <ButtonLayout>
+        <Button text="직원 로그인"/>
+        <Button text="고객 로그인"/>
+      </ButtonLayout>
+      <SignupDiv>
+        아직 계정이 없으신가요?
+        <StyledLink to='/Signup'>  회원가입</StyledLink>
+      </SignupDiv>
+    </MainDiv>
   );
 };
 
-export default Login;<h1>로그인</h1>
+export default Login;
