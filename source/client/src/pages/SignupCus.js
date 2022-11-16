e35bfee3b6fa41065ecdb9f2e23168ca9719fd2f
alicehrk <alicehrk23@gmail.com>
2022-11-16 15:24:53 +0900

    회원가입 local

--- source/client/src/pages/SignupCus.js
+++ source/client/src/pages/SignupCus.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, {css} from 'styled-components';
 import Input from '../components/Input';
 import Button from '../components/Button';
@@ -22,19 +22,88 @@ const InputLayer = styled.div`
 `
 
 const SignupCus = () => {
+	const [signUpInfo, setSignUpInfo] = useState({
+		name: '',
+		id: '',
+		password: '',
+		checkPassword: '',
+		email: '',
+		phone: '',
+		address: '',
+	});
+	const checkNotEmptySignUpInfoValue = Object.values(signUpInfo).some((data) => data === '');
+
+	const handleChangeSignUpInfoInput = (e) => {
+		const { name, value } = e.target;
+		setSignUpInfo((prev) => ({ ...prev, [name]: value }));
+	}
+
   return (
     <MainDiv>
-        <img src='/images/signup_cus.svg' alt='고객 회원가입' width={200}/>
-        <InputLayer>
-					<Input placeholder='이름'/>
-					<Input placeholder='아이디'/>
-					<Input placeholder='비밀번호'/>
-					<Input placeholder='비밀번호 재입력'/>
-					<Input placeholder='이메일 주소'/>
-                    <Input placeholder='휴대폰 번호'/>
-                    <Input placeholder='배달 주소'/>
-				</InputLayer>
-				<Button>회원가입</Button>
+      <img src="/images/signup_cus.svg" alt="고객 회원가입" width={200} />
+      <InputLayer>
+				{/* {checkNotEmptySignUpInfoValue && <div>모든 항목을 입력해주세요</div>} */}
+        <Input
+          placeholder="이름"
+          name={"name"}
+          value={signUpInfo.name}
+          onChange={handleChangeSignUpInfoInput}
+        />
+        <Input
+          placeholder="아이디"
+          name={"id"}
+          value={signUpInfo.id}
+          onChange={handleChangeSignUpInfoInput}
+        />
+        <Input
+          placeholder="비밀번호"
+          type="password"
+          name={"password"}
+          value={signUpInfo.password}
+          onChange={handleChangeSignUpInfoInput}
+        />
+        <Input
+          placeholder="비밀번호 재입력"
+          type="password"
+          name={"checkPassword"}
+          value={signUpInfo.checkPassword}
+          onChange={handleChangeSignUpInfoInput}
+        />
+        <Input
+          placeholder="이메일 주소"
+          name={"email"}
+          value={signUpInfo.email}
+          onChange={handleChangeSignUpInfoInput}
+        />
+        <Input
+          placeholder="휴대폰 번호"
+          name={"phone"}
+          value={signUpInfo.phone}
+          onChange={handleChangeSignUpInfoInput}
+        />
+        <Input
+          placeholder="배달 주소"
+          name={"address"}
+          value={signUpInfo.address}
+          onChange={handleChangeSignUpInfoInput}
+        />
+      </InputLayer>
+      <Button
+        onClick={() => {
+          console.log(signUpInfo);
+					if (checkNotEmptySignUpInfoValue) {
+						alert('모든 항목을 채워주세요.');
+						return;
+					}
+          if (signUpInfo.password !== signUpInfo.checkPassword) {
+            alert("비밀번호가 서로 다릅니다.");
+            return;
+          }
+        }}
+				//disabled={checkNotEmptySignUpInfoValue}
+      >
+        회원가입
+      </Button>
     </MainDiv>
   );
 };
